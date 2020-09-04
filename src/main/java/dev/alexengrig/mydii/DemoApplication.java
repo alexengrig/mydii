0adba4d09a4e95f429f28f0c97300bb13325fe5c
Grig Alex <alexengrigdev@gmail.com>
2020-09-05 00:16:46 +0300

    Add guice

--- src/main/java/dev/alexengrig/mydii/DemoApplication.java
+++ src/main/java/dev/alexengrig/mydii/DemoApplication.java
@@ -16,17 +16,42 @@
 
 package dev.alexengrig.mydii;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import dev.alexengrig.mydii.domain.DemoDomain;
 import dev.alexengrig.mydii.repository.DemoRepository;
 import dev.alexengrig.mydii.repository.PermanentDemoRepository;
 import dev.alexengrig.mydii.service.ConsoleDemoService;
 import dev.alexengrig.mydii.service.DemoService;
 
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+
 public class DemoApplication {
     public static void main(String[] args) {
-        DemoDomain domain = new DemoDomain();
-        DemoRepository repository = new PermanentDemoRepository(domain);
-        DemoService service = new ConsoleDemoService(repository);
+        Injector injector = Guice.createInjector(new DemoModule());
+        DemoService service = injector.getInstance(DemoService.class);
         service.demonstrate();
     }
+
+    static class DemoModule extends AbstractModule {
+        @Override
+        protected void configure() {
+            bind(DemoDomain.class).toInstance(new DemoDomain());
+            bind(DemoRepository.class).toConstructor(getConstructor(PermanentDemoRepository.class, DemoDomain.class));
+            bind(DemoService.class).toConstructor(getConstructor(ConsoleDemoService.class, DemoRepository.class));
+        }
+
+        private <T> Constructor<T> getConstructor(Class<T> type, Class<?>... parameters) {
+            try {
+                return type.getConstructor(parameters);
+            } catch (NoSuchMethodException e) {
+                throw new IllegalArgumentException(String.format(
+                        "No constructor in class '%s' with the following parameters: %s",
+                        type, Arrays.toString(parameters)), e);
+            }
+        }
+    }
+
 }
