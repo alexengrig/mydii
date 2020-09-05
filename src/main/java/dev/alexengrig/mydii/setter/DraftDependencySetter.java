e5bec5646b5e847fb315d55de6270a1c91533aad
Grig Alex <alexengrigdev@gmail.com>
2020-09-06 00:28:53 +0300

    Create DependencySetter and DraftDependencySetter

---
+++ src/main/java/dev/alexengrig/mydii/setter/DraftDependencySetter.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2020 Alexengrig Dev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.alexengrig.mydii.setter;
+
+import dev.alexengrig.mydii.DependencyStorage;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+public class DraftDependencySetter implements DependencySetter {
+    @Override
+    public void setDependencies(Object dependency, DependencyStorage storage) {
+        Class<?> type = dependency.getClass();
+        Field[] fields = type.getDeclaredFields();
+        for (Field field : fields) {
+            if (!Modifier.isFinal(field.getModifiers())) {
+                Object fieldDependency = storage.getDependency(field.getType());
+                try {
+                    if (!field.isAccessible()) {
+                        field.setAccessible(true);
+                    }
+                    field.set(dependency, fieldDependency);
+                } catch (IllegalAccessException e) {
+                    throw new IllegalStateException(e);
+                }
+            }
+        }
+    }
+}
