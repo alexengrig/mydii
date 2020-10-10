f2039220788fdd3b32e592e266189c4a2f39dd51
Grig Alex <alexengrigdev@gmail.com>
2020-10-10 22:34:34 +0300

    Refactor DraftDependencyFactory and add logger

--- src/main/java/dev/alexengrig/mydii/factory/DraftDependencyFactory.java
+++ src/main/java/dev/alexengrig/mydii/factory/DraftDependencyFactory.java
@@ -18,6 +18,8 @@ package dev.alexengrig.mydii.factory;
 
 import dev.alexengrig.mydii.DependencyStorage;
 import dev.alexengrig.mydii.finder.DependencyFinder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
@@ -26,35 +28,41 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class DraftDependencyFactory implements DependencyFactory {
+    private static final Logger LOG = LoggerFactory.getLogger(DraftDependencyFactory.class);
+
     @Override
     public <T> T createDependency(Class<T> type, DependencyStorage storage) {
+        LOG.debug("Started creation a dependency for type: {}", type.getName());
         Class<T> targetType = getTargetType(type, storage);
-        Constructor<T> constructor = getConstructor(targetType);
-        Parameter[] parameters = constructor.getParameters();
-        Object[] dependencies = getDependencies(parameters, storage);
-        try {
-            if (!constructor.isAccessible()) {
-                constructor.setAccessible(true);
-            }
-            return constructor.newInstance(dependencies);
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            throw new IllegalStateException(e);
-        }
+        T target = getInstance(targetType, storage);
+        LOG.debug("Finished creation dependency for type: {}", type.getName());
+        return target;
     }
 
     private <T> Class<T> getTargetType(Class<T> type, DependencyStorage storage) {
         if (!type.isInterface()) {
+            LOG.debug("Type is not an interface: {}", type.getName());
             return type;
         }
+        LOG.debug("Type is an interface: {}", type.getName());
         DependencyFinder finder = storage.getConfiguration().getFinder();
         List<Class<T>> implementations = finder.findImplementations(type);
         if (implementations.isEmpty()) {
             throw new IllegalArgumentException("No implementation for interface: " + type);
         } else if (implementations.size() > 1) {
             throw new IllegalArgumentException("Interface has more than 1 implementation: "
-                    + implementations.stream().map(Class::toString).collect(Collectors.joining(", ")));
+                    + implementations.stream().map(Class::getName).collect(Collectors.joining(", ")));
         }
-        return implementations.get(0);
+        Class<T> targetType = implementations.get(0);
+        LOG.debug("Target type: {}", targetType.getName());
+        return targetType;
+    }
+
+    private <T> T getInstance(Class<T> type, DependencyStorage storage) {
+        Constructor<T> constructor = getConstructor(type);
+        Parameter[] parameters = constructor.getParameters();
+        Object[] dependencies = getDependencies(parameters, storage);
+        return newInstance(constructor, dependencies);
     }
 
     private <T> Constructor<T> getConstructor(Class<T> type) {
@@ -63,7 +71,9 @@ public class DraftDependencyFactory implements DependencyFactory {
         if (constructors.length != 1) {
             throw new IllegalArgumentException("Class has more than 1 constructor: " + type);
         }
-        return constructors[0];
+        Constructor<T> constructor = constructors[0];
+        LOG.debug("Type constructor: {}", constructor);
+        return constructor;
     }
 
     private Object[] getDependencies(Parameter[] parameters, DependencyStorage storage) {
@@ -72,8 +82,22 @@ public class DraftDependencyFactory implements DependencyFactory {
         for (int i = 0; i < parameterCount; i++) {
             Parameter parameter = parameters[i];
             Class<?> parameterType = parameter.getType();
-            dependencies[i] = storage.getDependency(parameterType);
+            Object dependency = storage.getDependency(parameterType);
+            dependencies[i] = dependency;
+            LOG.debug("Type parameter with dependency: {} - {}",
+                    parameterType.getName(), dependency.getClass().getName());
         }
         return dependencies;
+    }
+
+    private <T> T newInstance(Constructor<T> constructor, Object[] dependencies) {
+        try {
+            if (!constructor.isAccessible()) {
+                constructor.setAccessible(true);
+            }
+            return constructor.newInstance(dependencies);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new IllegalArgumentException("Exception of creation new instance", e);
+        }
     }
 }
